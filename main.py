6745d5376f73daebad859cc49edc43a7098548ef
Norman Peitek <norman.peitek@gmail.com>
2017-12-08 14:35:23 +0100

    add parsing of physio data

--- main.py
+++ main.py
@@ -8,68 +8,115 @@ import re
 
 
 def main():
-    print("Starting script...")
+    #file_name = "eyetracking_zp65_2017_12_06.asc"
+    participant_id = "test"
+
+    all_lines = parse_eyetracking_data(participant_id)
+    all_lines = parse_physio_data(all_lines, participant_id)
+    write_csv_file(all_lines, participant_id)
+
+
+def parse_physio_data(all_lines, participant_id):
+    print("Parse physio data...")
+
+    physio_input_file_path = join("input", participant_id + ".log")
+
+    with open(physio_input_file_path) as input_file:
+        start = False
+        #todo correct the wrong resolution. currently we assume 500 hertz instead of the correct 496 hertz!
+        #todo also minimize that heartrate/breathing only has a resolution of 100 hertz, currently it's added too often without an effect
+        imestampMs = 0
+
+        for i, line in enumerate(input_file):
+            if (i % 10000) == 0:
+                print("-> Read row of physio: ", i)
+
+            if line.startswith("#"):
+                continue
+
+            elements = line.split(" ")
+
+            if not start:
+                mark = elements[12]
+
+                if mark[0] == '1':
+                    print("-> Found first frame of at line physio: ", i)
+                    start = True
+
+            if start:
+                imestampMs += 2
+                heartrate = elements[6]
+                breathing = elements[7]
+
+                try:
+                    all_lines[imestampMs]["HeartRate"] = heartrate
+                    all_lines[imestampMs]["Breathing"] = breathing
+                except:
+                    print("Exception: more physio data than eyetracking! This shouldn't happen...")
+                    break
+
+    return all_lines
+
+
+def parse_eyetracking_data(participant_name):
+    print("Parse eyetracking data...")
+
+    eyetracking_input_file_path = join("input", participant_name + ".asc")
 
     # read in eyelink file line for line
-    input_file_path = "C:/Users/npeitek/Documents/GitHub/EyeLinkOgamaConnector/input/eyetracking_zp65_2017_12_06.asc"
-
     all_lines = []
 
-    # create appropriate object with all information (subject, ...)
-
-    carry_over = {
-        "Subject": "",
-        "Snippet": "",
-        "TrialImage": "",
-        "TrialNumber": ""
-    }
-
     print("Reading file...")
-    # check whether line is header, MSG or eyetracking file
-    with open(input_file_path) as input_file:
+    with open(eyetracking_input_file_path) as input_file:
         frames_size_code_counter = 0
+        trial_image = ""
         trial_number = 0
         d2_task_number = 1
         dec_time_number = 1
         rest_number = 1
+        timestamp = 1
+        subject = participant_name
+        snippet = ""
 
         for i, line in enumerate(input_file):
-            if (i % 5000) == 0:
-                print("-> row ", i)
+            if (i % 25000) == 0:
+                print("-> Read row of eyetracking: ", i)
 
             csv_line_object = {
-                "Id": i,
-                "Subject": "zp65", #carry_over["Subject"],
-                "Snippet": carry_over["Snippet"],
-                "TrialImage": carry_over["TrialImage"],
+                "Line": i,
+                "Subject": subject,
+                "Snippet": snippet,
+                "TrialImage": trial_image,
                 "TrialNumber": trial_number,
                 "Time": None,
+                "TimestampMs": timestamp,
                 "GazePosX": None,
                 "GazePosY": None,
+                "HeartRate": None,
+                "Breathing": None,
             }
 
             # if the current one is code
-            # todo detect specific d2 image
-            if not carry_over["Snippet"].startswith("D2") and not carry_over["Snippet"].startswith("Rest") and not carry_over["Snippet"].startswith("DecTime"):
+            if not snippet.startswith("D2") and not snippet.startswith("Rest") and not snippet.startswith("DecTime"):
                 if frames_size_code_counter >= 30000:
                     print("--> switching to decision time after frames: ", frames_size_code_counter)
-                    carry_over["Snippet"] = "DecTime" + str(dec_time_number)
-                    carry_over["TrialImage"] = "dec_time_" + str(dec_time_number) + ".png"
+                    snippet = "DecTime" + str(dec_time_number)
+                    trial_image = "dec_time_" + str(dec_time_number) + ".png"
                     frames_size_code_counter = 0
                     trial_number += 1
                     dec_time_number += 1
-            elif not carry_over["Snippet"].startswith("D2") and not carry_over["Snippet"].startswith("Rest"):
+            elif not snippet.startswith("D2") and not snippet.startswith("Rest"):
                 if frames_size_code_counter >= 2000:
                     print("--> switching to d2 after frames: ", frames_size_code_counter)
-                    carry_over["Snippet"] = "D2_" + str(d2_task_number)
-                    carry_over["TrialImage"] = "attention_task_" + str(d2_task_number) + ".png"
+                    snippet = "D2_" + str(d2_task_number)
+                    trial_image = "attention_task_" + str(d2_task_number) + ".png"
                     frames_size_code_counter = 0
                     trial_number += 1
                     d2_task_number += 1
-            elif carry_over["Snippet"].startswith("D2") and frames_size_code_counter >= 15000:
+            elif not snippet.startswith("D2") and frames_size_code_counter >= 15000:
                 print("--> switching to rest after frames: ", frames_size_code_counter)
-                carry_over["Snippet"] = "Rest" + str(rest_number)
-                carry_over["TrialImage"] = "rest_" + str(rest_number) + ".png"
+                snippet = "Rest" + str(rest_number)
+                trial_image = "rest_" + str(rest_number) + ".png"
                 frames_size_code_counter = 0
                 trial_number += 1
                 rest_number += 1
@@ -83,13 +130,14 @@ def main():
                 #if (i % 500) == 0:
                 all_lines.append(csv_line_object)
                 frames_size_code_counter += 1
+                timestamp += 1
 
             elif line[0].isalpha():
                 if "!V IMGLOAD FILL" in line:
                     startpos = line.rfind("\\")
                     snippet = line[startpos+1:].replace('\r', '').replace('\n', '')
-                    carry_over["TrialImage"] = snippet
-                    carry_over["Snippet"] = snippet
+                    trial_image = snippet
+
                     print("--> found snippet " + snippet + " after frames ", frames_size_code_counter)
                     frames_size_code_counter = 0
                     trial_number += 1
@@ -98,15 +146,23 @@ def main():
                     #carry_over["TrialNumber"] = line[startpos+14:].replace('\r', '').replace('\n', '')
                 elif "Subject" in line:
                     startpos = line.find("Subject")
-                    carry_over["Subject"] = line[startpos+8:].replace('\r', '').replace('\n', '')
+                    subject = line[startpos+8:].replace('\r', '').replace('\n', '')
 
     print("-> reading file: done!")
 
-    print("Save objects to file...")
+    return all_lines
+
+
+def write_csv_file(all_lines, participant_name):
+    print("\n====================\n")
+    print("Final step: Write everything to a csv file...")
+
     # write objects to file as giant csv
-    output_file_path = "C:/Users/npeitek/Documents/GitHub/EyeLinkOgamaConnector/output/eyetracking_zp65_2017_12_06.csv"
+    output_file_path = join("output", participant_name + ".csv")
     with open(output_file_path, 'w') as output_file:
-        output_file.write("Id")
+        output_file.write("Line")
+        output_file.write(';')
+        output_file.write("TimestampMs")
         output_file.write(';')
         output_file.write("Time")
         output_file.write(';')
@@ -121,13 +177,19 @@ def main():
         output_file.write("GazePosX")
         output_file.write(';')
         output_file.write("GazePosY")
+        output_file.write(';')
+        output_file.write("HeartRate")
+        output_file.write(';')
+        output_file.write("Breathing")
 
         for i, line in enumerate(all_lines):
-            if (i % 5000) == 0:
+            if (i % 25000) == 0:
                 print("-> row ", i)
 
             output_file.write('\n')
-            output_file.write(str(line["Id"]))
+            output_file.write(str(line["Line"]))
+            output_file.write(';')
+            output_file.write(str(line["TimestampMs"]))
             output_file.write(';')
             output_file.write(str(line["Time"]))
             output_file.write(';')
@@ -142,7 +204,10 @@ def main():
             output_file.write(str(line["GazePosX"]))
             output_file.write(';')
             output_file.write(str(line["GazePosY"]))
-
+            output_file.write(';')
+            output_file.write("" if line["HeartRate"] is None else str(line["HeartRate"]))
+            output_file.write(';')
+            output_file.write("" if line["Breathing"] is None else str(line["Breathing"]))
     print("-> saving file: done!")
 
 
